17b7f32f3c0b227f7670e6d4a2f963917f69b31e
Naveen <naveenfaster@gmail.com>
2022-10-17 15:31:16 +0530

    updated setup and init_setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,7 @@
 echo [$(date)]: "START"
 export _VERSION_=3.8
 echo [$(date)]: "creating environment with python ${_VERSION_}"
-conda create --prefix ./env python=${_VERSION_} -y
+conda create --prefix ./venv python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "<github repo name>" # github repo name
-AUTHOR_USER_NAME = "c17hawke" # update as per your need
-SRC_REPO = "<change me>" # example sklearn etc
+REPO_NAME = "California_Housing_DVC" # github repo name
+AUTHOR_USER_NAME = "nav52" # update as per your need
+AUTHOR_EMAIL = "naveenfaster@gmail.com"
+SRC_REPO = "California_Housing_DVC" # example sklearn etc
 LIST_OF_REQUIREMENTS = [] # core dependencies
 
 
@@ -18,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email=AUTHOR_EMAIL,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     license="MIT",
